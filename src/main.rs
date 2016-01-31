ed32bfc10a8685ce376b477c9131a6cef6dcdc33
Bryan Mytko <bryanmytko@gmail.com>
2016-01-31 00:03:18 -0500

    Add router

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,25 @@
 extern crate iron;
+extern crate router;
 
 use iron::prelude::*;
 use iron::status;
+use router::Router;
 
 fn main() {
-  Iron::new(|_: &mut Request|{
-    Ok(Response::with((status::Ok, "Hello Universe!")))
-  }).http("localhost:3000").unwrap();
+
+  let mut router = Router::new();
+
+  router.get("/", hello_world);
+  router.get("/test", test);
+
+  fn hello_world(_: &mut Request) -> IronResult<Response>{
+    let greeting = "Hello Universe!";
+    Ok(Response::with((status::Ok, greeting)))
+  }
+
+  fn test(_: &mut Request) -> IronResult<Response>{
+    Ok(Response::with((status::Ok, "hi")))
+  }
+
+  Iron::new(router).http("localhost:3000").unwrap();
 }
